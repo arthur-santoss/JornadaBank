6c50a19916012fba04b8aa3b66360dd841bc3a5f
Arthur dos Santos <67018260+Arthur-San@users.noreply.github.com>
2023-05-25 09:56:15 -0300

    Update Ex05.java
    correção na soma das colunas

--- src/Modulo03/Ex05.java
+++ src/Modulo03/Ex05.java
@@ -1,32 +1,33 @@
 package Modulo03;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Ex05 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
-		double[][] matriz = {
-	            {1.0, 1.0, 1.0},
-	            {2.5, 2.5, 2.5},
-	            {3.7, 3.7, 3.7}
-	        };
-		
-		//imprimir
-		
-		for (int coluna = 0; coluna < matriz[0].length; coluna++) {
-			double somaColuna = 0.0;
-			
-			for (int linha = 0; linha < matriz.length; linha++) {
-				System.out.print(matriz[coluna][linha] + " | ");
-				somaColuna += matriz[linha][coluna];
-			}
-			System.out.println("Soma da coluna " + coluna + ": "+ somaColuna);
-		}
-		
-		
-		sc.close();
+
+        double[][] matriz = {
+                {1.0, 2.5, 3.7},
+                {1.0, 2.5, 3.7},
+                {1.0, 2.5, 3.7}
+        };
+
+        // imprimir
+        DecimalFormat df = new DecimalFormat("#.0");
+
+        for (int coluna = 0; coluna < matriz[0].length; coluna++) {
+            double somaColuna = 0.0;
+
+            for (int linha = 0; linha < matriz.length; linha++) {
+                System.out.print(df.format(matriz[linha][coluna]) + " | ");
+                somaColuna += matriz[linha][coluna];
+            }
+            System.out.println("Soma da coluna " + coluna + ": " + df.format(somaColuna));
+        }
+
+        sc.close();
 	}
 
 }
