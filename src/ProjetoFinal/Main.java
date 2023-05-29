4c40d7feaff56a371b40d543a8e80440837f3ae8
Arthur-San <zthw01@gmail.com>
2023-05-28 21:58:05 -0300

    inserção de opção de criar usuário e mostrar todos usuários cadastrados

--- src/ProjetoFinal/Main.java
+++ src/ProjetoFinal/Main.java
@@ -1,46 +1,86 @@
 package ProjetoFinal;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		String[][] matrizUsuarios = new String [5][4];
-		adicionarUsuario(matrizUsuarios);
-
-		// imprimir matriz
-		System.out.println("\nDados da Matriz:");
-		for (int i = 0; i < matrizUsuarios.length; i++) {
-			for (int j = 0; j < matrizUsuarios[i].length; j++) {
-				System.out.print(matrizUsuarios[i][j] + " ");
+		Scanner ler = new Scanner(System.in);
+		
+		int opcao = -1;
+		String[][] matrizUsuarios = new String [4][5];
+		
+		
+		do {
+			System.out.print(
+					"\nEscolha uma opção: \n"
+					+ "1) criar conta:\n"
+					+ "2) Fazer depósito\n"
+					+ "3) Fazer Saque\n"
+					+ "4) Fazer transferência\n"
+					+ "5) Mostrar contas cadastradas\n"
+					+ "0) SAIR\n"
+					+ "->");
+			
+			opcao = ler.nextInt();
+			switch (opcao) {
+			case 1:
+				adicionarUsuario(matrizUsuarios);
+				break;
+			
+			case 5:
+				mostrarContasCadastradas(matrizUsuarios);
+				break;
+				
+			default:
+				break;
 			}
-			System.out.println();
-		}
-		// imprime a posição vazia na matriz
-		System.out.println("numero da conta: " + numeroDaconta() );
-
+			
+		} while (opcao != 0);	
+		
+		ler.close();
 	}
 
-	private static String numeroDaconta() {
-		// TODO Auto-generated method stub
-		return null;
+	
+
+	private static void mostrarContasCadastradas(String[][] matrizUsuarios) {
+		// imprimir matriz
+				System.out.println("\nDados da Matriz:\n"
+						+ "nome / CPF / E-mail / Senha / Conta ");
+				for (int i = 0; i < matrizUsuarios.length; i++) {
+					for (int j = 0; j < matrizUsuarios[i].length; j++) {
+						System.out.print(matrizUsuarios[i][j] + " | ");
+					}
+					System.out.println();
+				}
 	}
+
+
 
 	public static void adicionarUsuario(String[][] matrizUsuarios) {
-		int posicao = encontrarPosicaoVazia(matrizUsuarios);
 		Scanner ler = new Scanner(System.in);
+		Random random = new Random();
+		
+		String numeroAleatorio = String.valueOf(random.nextInt(1000,5000));
+		int posicao = encontrarPosicaoVazia(matrizUsuarios);
+		
+		
 
-		System.out.println("digite seu nome: ");
+		System.out.print("\ndigite seu nome: ");
 		matrizUsuarios[posicao][0] = ler.next();
 		
-		System.out.println("digite seu cpf: ");
+		System.out.print("digite seu cpf: ");
 		matrizUsuarios[posicao][1] = ler.next();
 		
-		System.out.println("digite seu email: ");
+		System.out.print("digite seu email: ");
 		matrizUsuarios[posicao][2] = ler.next();
 		
-		System.out.println("digite seu senha: ");
+		System.out.print("digite seu senha: ");
 		matrizUsuarios[posicao][3] = ler.next();
-
+		
+		//adiciona numero da contaz
+		matrizUsuarios[posicao][4] = numeroAleatorio;
+		
 	}
 
 	public static int encontrarPosicaoVazia(String[][] matrizUsuarios) {
